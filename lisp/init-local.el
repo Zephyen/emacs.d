cba1db7222da2933f7183d8e7b4bf2e1e5de9778
Zero <305737657@qq.com>
2018-08-09 03:07:54 +0800

    隐藏dired里面的无用信息

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -29,5 +29,11 @@
         (delq (current-buffer)
               (remove-if-not 'buffer-file-name (buffer-list)))))
 
+(defun xah-dired-mode-setup ()
+  "to be run as hook for 'dired-mode'."
+  (dired-hide-details-mode 1))
+(setq dired-dwim-target t)
+(add-hook 'dired-mode-hook 'xah-dired-mode-setup)
+
 
 (provide 'init-local)
